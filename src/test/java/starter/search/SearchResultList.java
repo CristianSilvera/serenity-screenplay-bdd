85ce3a0865652c84898cf46b56a1cf276583b66c
Cristian Silvera <cristiansilveraa@gmail.com>
2024-02-19 00:25:07 -0300

    doc: serenity cucumber 4 starter - The issues have been resolved

---
+++ src/test/java/starter/search/SearchResultList.java
@@ -0,0 +1,8 @@
+package starter.search;
+
+import org.openqa.selenium.By;
+
+class SearchResultList {
+    static By RESULT_TITLES = By.xpath("//span[text()='7 Health Benefits of Eating Cucumber']");
+//    static By RESULT_TITLES = By.xpath("//h3[text()='Cucumber: BDD Testing & Collaboration Tools for Teams']");
+}
